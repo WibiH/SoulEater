51056e05044977a1192cb4c6ed2ddb74adc099b8
Wiebke Hagenauer <wiebosaurus@hotmail.com>
2023-03-13 22:33:33 +0100

    add styles - buttons, graphics

--- js_files/game.js
+++ js_files/game.js
@@ -18,7 +18,7 @@ class Game {
 
     this.setControls();
     this.isPaused = false;
-    this.soundtrack = new Audio("/sound/Soul Eater_2.wav"); // from SonjaDeffner|Kalme
+    this.soundtrack = new Audio("/sound/SoulEater.wav"); // from SonjaDeffner|Kalme
     this.soundtrack.loop = true;
     this.soundtrack.volume = 1;
     this.gameOverNoiseURL = "/sound/soulEater_gameOver.wav"; // from SonjaDeffner|Kalme
