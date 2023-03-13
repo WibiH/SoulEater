99acefa2fdea50c004e5ff93134ec7f6fbf0b930
Wiebke Hagenauer <wiebosaurus@hotmail.com>
2023-03-13 04:47:59 +0100

    update sountrack

--- js_files/game.js
+++ js_files/game.js
@@ -18,7 +18,7 @@ class Game {
 
     this.setControls();
     this.isPaused = false;
-    this.soundtrack = new Audio("/sound/soulEater_Soundtrack.wav"); // from SonjaDeffner|Kalme
+    this.soundtrack = new Audio("/sound/Soul Eater_LANG_1.wav"); // from SonjaDeffner|Kalme
     this.soundtrack.loop = true;
     this.soundtrack.volume = 1;
     this.gameOverNoiseURL = "/sound/soulEater_gameOver.wav"; // from SonjaDeffner|Kalme
